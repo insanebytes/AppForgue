a463c555364fb129f6dfa306f71a65b849f207b0
Andres <arosado@une.org>
2024-12-13 13:47:09 +0100

    updated imgui commit and changes to code.

--- TestApp/src/main.cpp
+++ TestApp/src/main.cpp
@@ -1,34 +1,42 @@
 #include "AppForgue.h"
 #include <format>
 
-
-void mainLoop(Windowing::Window* window)
+void drawTitleBar(Windowing::Window* window)
 {
 	const float titlebarHeight = 58.0f;
-	auto* fgDrawList = ImGui::GetForegroundDrawList();
-
 	const bool isMaximized = window->IsMaximized();
+	const float windowBorderSize = ImGui::GetCurrentWindow()->WindowBorderSize;
 	float titlebarVerticalOffset = isMaximized ? -6.0f : 0.0f;
-	const ImVec2 windowPadding = ImGui::GetCurrentWindow()->WindowPadding;
 
-	ImGui::SetCursorPos(ImVec2(windowPadding.x, windowPadding.y + titlebarVerticalOffset));
+	ImGui::SetCursorPos(ImVec2(windowBorderSize, titlebarVerticalOffset));
 	const ImVec2 titlebarMin = ImGui::GetCursorScreenPos();
-	const ImVec2 titlebarMax = { ImGui::GetCursorScreenPos().x + ImGui::GetWindowWidth() - windowPadding.y * 2.0f,
-								 ImGui::GetCursorScreenPos().y + titlebarHeight };
+	const ImVec2 titlebarMax = { ImGui::GetCursorScreenPos().x + ImGui::GetWindowWidth() - (windowBorderSize * 2.0f),ImGui::GetCursorScreenPos().y + titlebarHeight };
 
-	fgDrawList->AddRect(titlebarMin, titlebarMax, ImColor::ImColor(255,0,0));
+	ImDrawList* fgDrawList = ImGui::GetForegroundDrawList();
+	fgDrawList->AddRectFilled(titlebarMin, titlebarMax, ImColor::ImColor(24, 24, 24));
+
+	ImGui::BeginHorizontal("Titlebar", { ImGui::GetWindowWidth() - windowBorderSize * 2.0f, ImGui::GetFrameHeightWithSpacing() });
 
 	const float w = ImGui::GetContentRegionAvail().x;
 	const float buttonsAreaWidth = 94;
-	ImGui::InvisibleButton("##titleBarDragZone", ImVec2(w - buttonsAreaWidth, titlebarHeight));
-
+	ImGui::InvisibleButton("##titleBarDragZone", ImVec2(w - buttonsAreaWidth, 58.0f));
 	window->SetTitleBarHitted(ImGui::IsItemHovered());
-
-	ImGui::Begin("Ventana Aplicacion", NULL);
-	ImGui::Text(std::format("Abrir {}", ICON_FA_FOLDER).c_str());
-	ImGui::End();
+	
+	ImGui::Spring();
+	ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 8.0f);
+	ImGui::Button(ICON_FA_WINDOW_MINIMIZE, ImVec2(30, 30));
+	//ImGui::Button(ICON_FA_WINDOW_MAXIMIZE, ImVec2(30, 30));
+	ImGui::EndHorizontal();
 }
 
+void mainLoop(Windowing::Window* window)
+{
+	drawTitleBar(window);
+
+	//ImGui::Begin("Ventana Aplicacion", NULL);
+	//ImGui::Text(std::format("Abrir {}", ICON_FA_FOLDER).c_str());
+	//ImGui::End();
+}
 
 void createApplication(int argc, char** argv)
 {
@@ -46,7 +54,7 @@ void createApplication(int argc, char** argv)
 #if defined(PLATFORM_WINDOWS)
 #include <Windows.h>
 
-int APIENTRY WinMain(_In_ HINSTANCE hInstance,_In_opt_ HINSTANCE hPrevInstance,_In_ LPSTR lpCmdLine,_In_ int nShowCmd)
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
 	createApplication(__argc, __argv);
 }
